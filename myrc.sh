7bfa420f9e06a267f7abebec37e6de594c9a4609
Jaemin Choi <jaemin.choi@bearrobotics.ai>
2024-10-21 20:27:21 -0700

    bazel fzf

--- myrc.sh
+++ myrc.sh
@@ -1,5 +1,6 @@
 alias vim="nvim"
 alias vi="nvim"
+alias lg="lazygit"
 
 # Set up fzf key bindings and fuzzy completion
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
@@ -59,3 +60,31 @@ function prompt_command() {
 }
 
 export PROMPT_COMMAND='prompt_command'
+
+# Bazel fzf
+_fzf_complete_bazel_test() {
+    _fzf_complete '-m' "$@" < <(command bazel query \
+        "kind('(test|test_suite) rule', //...)" 2>/dev/null)
+}
+
+_fzf_complete_bazel() {
+    local tokens
+    tokens=($(echo "$COMP_LINE"))
+
+    if [ ${#tokens[@]} -ge 3 ] && [ "${tokens[2]}" = "test" ]; then
+        _fzf_complete_bazel_test "$@"
+    else
+        # Might be able to make this better someday, by listing all repositories
+        # that have been configured in a WORKSPACE.
+        # See https://stackoverflow.com/questions/46229831/ or just run
+        #     bazel query //external:all
+        # This is the reason why things like @ruby_2_6//:ruby.tar.gz don't show up
+        # in the output: they're not a dep of anything in //..., but they are deps
+        # of @ruby_2_6//...
+        _fzf_complete '-m' "$@" < <(command bazel query --keep_going \
+            --noshow_progress \
+            "kind('(binary rule)|(generated file)', deps(//...))" 2>/dev/null)
+    fi
+}
+
+[ -n "$BASH" ] && complete -F _fzf_complete_bazel -o default -o bashdefault bazel
