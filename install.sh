47b9b6924c3dd2388acaf1c3cb49c586059f48ed
Jaemin Choi <jaemin.choi@bearrobotics.ai>
2024-10-24 16:33:12 -0700

    setup tmux

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+# TODO: check os (only support linux & mac)
+# TODO: check sudo permission
+# TODO: clone repo to specific directory
+# TODO: set basedir to the repo directory
 BASEDIR=$(realpath $(dirname $0))
 echo "BASEDIR: $BASEDIR"
 
 # Add entry script
-grep -q "$BASEDIR/myrc.sh" ~/.bashrc || echo "source ~/.jaemin/myrc.sh" >>~/.bashrc
+# TODO: replace bash with zsh
+if ! grep -q "source $BASEDIR/setup.sh" $HOME/.bashrc; then
+    echo "source $BASEDIR/setup.sh" >>$HOME/.bashrc
+fi
 
 # Install homebrew (Mac only)
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -21,20 +28,53 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install neovim
 fi
 
-ln -sf ~/.config/nvim $BASEDIR/nvim
+if [ -d $HOME/.config/nvim ]; then
+    if [ "$(readlink $HOME/.config/nvim)" != "$BASEDIR/nvim" ]; then
+        echo "ERROR: $HOME/.config/nvim exists" >&2
+        exit 1
+    fi
+else
+    ln -sf "$BASEDIR/nvim" "$HOME/.config/nvim"
+fi
 
-# TODO: install fzf
-# TODO: install & setup tmux
+# Install fzf
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if [ ! -d $HOME/.fzf ]; then
+        git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+        $HOME/.fzf/install
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install fzf
+fi
 
-ln -sf ~/.config/tmux $BASEDIR/tmux
+# Install lazygit
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    rm lazygit lazygit.tar.gz
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install lazygit
+fi
 
-# TODO: install lazygit
+# Install tmux
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt install tmux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install tmux
+fi
 
-##### TMUX #####
+if [ -d $HOME/.config/tmux ]; then
+    if [ "$(readlink $HOME/.config/tmux)" != "$BASEDIR/tmux" ]; then
+        echo "ERROR: $HOME/.config/nvim exists" >&2
+        exit 1
+    fi
+else
+    ln -sf "$BASEDIR/tmux" "$HOME/.config/tmux"
+fi
 
 # Install tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Install tmux plugins
-git clone https://github.com/wfxr/tmux-power ~/.tmux/plugins/tmux-power
-git clone https://github.com/wfxr/tmux-net-speed ~/.tmux/plugins/tmux-net-speed
+if [ ! -d $HOME/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
