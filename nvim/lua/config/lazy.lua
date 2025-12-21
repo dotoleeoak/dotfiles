b6c58a9105017b29fd534705b5ca7b4e5e8a4344
Jaemin Choi <1dotolee@gmail.com>
2025-12-21 14:11:13 +0900

    chore: do not use jdtls

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -69,18 +69,6 @@ require("lazy").setup({
             end,
         },
         {
-            -- Java LSP
-            "mfussenegger/nvim-jdtls",
-            ft = { "java" },
-            config = function()
-                local config = {
-                    cmd = { "/opt/homebrew/bin/jdtls" },
-                    root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-                }
-                require("jdtls").start_or_attach(config)
-            end,
-        },
-        {
             -- Prisma LSP
             "prisma/vim-prisma",
         },
