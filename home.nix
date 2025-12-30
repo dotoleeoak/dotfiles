011b6b4a6876481c2c7ad81807390c44b75e286f
Jaemin Choi <1dotolee@gmail.com>
2025-12-31 02:31:11 +0900

    chore: enable zsh autosuggestion

--- home.nix
+++ home.nix
@@ -51,6 +51,8 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     initContent = "bindkey -v";
 
     shellAliases = {
